bc54304e0ca9a9c126ea117d880a5765442ad070
Andrew Kosenko <32103217+ageoldrain@users.noreply.github.com>
2024-09-05 18:30:55 -0400

    Update models.py

--- coin_flip/models.py
+++ coin_flip/models.py
@@ -28,7 +28,7 @@ Curiosity and Information Demand
 debug = False
 
 class C(BaseConstants):
-    NAME_IN_URL = 'Economics Experiment'
+    NAME_IN_URL = 'economics_experiment'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 10
    # NUM_PRACTICE_ROUNDS=2
