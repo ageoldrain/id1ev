af41ab7db4ca3f855bf0ca1f210fe3b063943d36
Andrew Kosenko <32103217+ageoldrain@users.noreply.github.com>
2024-10-28 12:16:54 -0400

    Update pages.py

--- coin_flip/pages.py
+++ coin_flip/pages.py
@@ -115,4 +115,4 @@ class Results(Page):
     def is_displayed(self):
         return self.round_number == C.NUM_ROUNDS
 
-page_sequence = [Introduction, Introduction1point5, Introduction1point5, Introduction2, ChooseCoin, RevealCoinOutcome, ChoosePermutation, Results]
+page_sequence = [Introduction, Introduction1point5, Introduction1point6, Introduction2, ChooseCoin, RevealCoinOutcome, ChoosePermutation, Results]
