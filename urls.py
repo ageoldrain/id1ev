44f991d3e1970efbcea5a97c4bf89ff19c10dbb0
Andrew Kosenko <32103217+ageoldrain@users.noreply.github.com>
2025-04-17 15:35:01 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Example for including app-specific URLs:
-    # path('', include('id1ev.urls')), 
+    # Include app-specific URLs
+    path('', include('id1ev.urls')),  # Make sure the app's name is `id1ev` and it has a `urls.py` file
 ]
