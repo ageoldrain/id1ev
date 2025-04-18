d546d35b00573e366595363dbe534b0f421250e6
Andrew Kosenko <32103217+ageoldrain@users.noreply.github.com>
2025-04-18 18:04:29 -0400

    Update urls.py

--- id1ev/urls.py
+++ id1ev/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # keep admin
     path('admin/', admin.site.urls),
-    path('', include('otree.urls')),  # include by string, not otree.urls module
+
+    # mount all oTree apps at the root, via our shim
+    path('', include('id1ev.otree_urlconf')),
 ]
