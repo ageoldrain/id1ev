24ffc3240958ce2f2d185ca714d24262c281cc89
Andrew Kosenko <32103217+ageoldrain@users.noreply.github.com>
2025-04-18 16:27:22 -0400

    Update urls.py

--- id1ev/urls.py
+++ id1ev/urls.py
@@ -1,8 +1,9 @@
+# id1ev/id1ev/urls.py
 from django.contrib import admin
 from django.urls import path, include
+import otree.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Include app-specific URLs
-    path('', include('id1ev.urls')),  # Make sure the app's name is `id1ev` and it has a `urls.py` file
+    path('', include(otree.urls)),   # this hands off to oTree’s built‑in URL router
 ]
