13c99b7dcda063ee0cb542ef8410a3517b8be478
Andrew Kosenko <32103217+ageoldrain@users.noreply.github.com>
2025-04-18 17:52:58 -0400

    Update urls.py

--- id1ev/urls.py
+++ id1ev/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Django admin interface
     path('admin/', admin.site.urls),
-
-    # Mount all oTree apps (as defined in SESSION_CONFIGS) at the site root
-    path('', include('otree.urls')),
+    path('', include('otree.urls')),  # include by string, not otree.urls module
 ]
